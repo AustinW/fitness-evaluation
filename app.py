9ec0aeac7184baf2ae0e25e78629d8cbe05dee87
Austin White <austingym@gmail.com>
2015-03-03 01:00:54 -0800

    Testing heroku postgresql integration

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.config.from_envvar('ENVIRONMENT_CONFIG', silent=True)
 
 db = SQLAlchemy(app)
---
+++ config_production.py
@@ -0,0 +1,1 @@
+SQLALCHEMY_DATABASE_URI = 'postgres://yevlldejcvctlu:_wC3E4Q3K1h9LggXsvz6dSxBST@ec2-23-23-180-133.compute-1.amazonaws.com:5432/d98cl07f2or2rm'
